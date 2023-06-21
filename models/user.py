0ac7546fb5f6ea6e4919099106e4581b6ee86104
LabyKreative <labykreative@yahoo.com>
2023-06-21 13:09:17 -0700

    DBStorage - User

--- models/user.py
+++ models/user.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""This is the user class"""
+"""Defines the user class"""
 from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 from models.place import Place
 from models.review import Review
 
