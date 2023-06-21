ac03f389e9ddd18c62c6949ef1afe9f31d84d685
LabyKreative <labykreative@yahoo.com>
2023-06-21 12:18:04 -0700

    Update models __init__

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""create a unique FileStorage instance for your application"""
+"""Defines and creates a unique FileStorage instance for the App"""
+from os import getenv
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
-from models.amenity import Amenity
 from models.place import Place
+from models.amenity import Amenity
 from models.review import Review
-from os import getenv
 
 
 if getenv("HBNB_TYPE_STORAGE") == "db":
