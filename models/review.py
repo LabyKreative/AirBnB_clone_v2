78784ff72877e70cf21cbce3e324755d2c23885f
LabyKreative <labykreative@yahoo.com>
2023-06-21 13:41:20 -0700

    DBStorage - Review

--- models/review.py
+++ models/review.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""This is the review class"""
+"""Defines the review class"""
 from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from models.base_model import BaseModel, Base
 
 
 class Review(BaseModel, Base):
