ee3090836cf6f65e051500247e4904136baff6d2
LabyKreative <labykreative@yahoo.com>
2023-07-07 02:37:28 -0700

    Update7: Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,18 +9,17 @@ from fabric.api import local
 def do_pack():
     """Create an archive file of the directory web_static directory"""
     dt = datetime.utcnow()
-    tar_file = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second)
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second)
     if local("mkdir -p versions").failed:
         return None
 
-    result = local("tar -cvzf {} web_static".format(tar_file))
+    result = local("tar -cvzf {} web_static".format(file))
     if result.failed:
         return None
 
-    size_result = local("wc -c {}".format(tar_file), capture=True)
+    size_result = local("wc -c {}".format(file), capture=True)
     size = size_result.stdout.split()[0]
 
-    print("web_static packed: {} -> {} Bytes".format(tar_file, size))
+    print("web_static packed: {} -> {} Bytes".format(file, size))
 
-    return tar_file
+    return file
