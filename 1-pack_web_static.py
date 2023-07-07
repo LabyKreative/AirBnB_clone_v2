c0d9b23b6c30917b422f7418cb20fcd33e4eb04b
LabyKreative <labykreative@yahoo.com>
2023-07-07 01:32:26 -0700

    Update5: Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-# a Fabric script that generates a .tgz archive from the contents of the
-# web_static folder of your AirBnB Clone repo, using the function do_pack.
+# a Fabric script that generates a .tgz archive from web_static dir.
 import os.path
 from datetime import datetime
 from fabric.api import local
 
 
 def do_pack():
-    """Create a .tgz archive from the contents of the web_static folder."""
-    now = datetime.now()
-    archive_name = "web_static_{}.tgz".format(now.strftime("%Y%m%d%H%M%S"))
-    archive_path = "versions/{}".format(archive_name)
-    if not os.path.exists("versions"):
-        os.makedirs("versions")
-    result = local("tar -czvf {} web_static".format(archive_path))
-    if result.succeeded:
-        return archive_path
-    return None
+    """Create an archive file of the directory web_static directory"""
+    dt = datetime.utcnow()
+    tar_file = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second)
+    if local("mkdir -p versions").failed:
+        return None
+
+    result = local("tar -cvzf {} web_static".format(tar_file))
+    if result.failed:
+        return None
+
+    size_result = local("wc -c {}".format(tar_file), capture=True)
+    size = size_result.stdout.split()[0]
+
+    print("web_static packed: {} -> {} Bytes".format(tar_file, size))
+
+    return tar_file
