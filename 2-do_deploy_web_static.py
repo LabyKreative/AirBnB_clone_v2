54f3e60ea111a85420cae192ad81193e3567f1b5
LabyKreative <labykreative@yahoo.com>
2023-07-09 13:01:50 +0000

    Update6: Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,42 +1,59 @@
 #!/usr/bin/python3
 # a Fabric script (based on the file 1-pack_web_static.py) that distributes
 # an archive to your web servers, using the function do_deploy:
-import os.path
+import os
+import argparse
+from os.path import exists, isfile
 from fabric.api import env, put, run
-from datetime import datetime
 
 env.hosts = ["100.26.154.241", "35.153.194.206"]
 
 
 def do_deploy(archive_path):
-    """Sends an archive to a web server."""
-    if os.path.isfile(archive_path) is False:
+    """Sends an archive to a web server"""
+    if not exists(archive_path) and not isfile(archive_path):
         return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+    try:
+        archive_filename = os.path.basename(archive_path)
+        no_ext = os.path.splitext(archive_filename)[0]
+
+        # Uploads the the archive to /tmp/ dir of the web server
+        put(archive_path, '/tmp/')
+
+        # Unzip the archive to the specified dir
+        release_folder = '/data/web_static/releases/' + no_ext + '/'
+        run('mkdir -p {}'.format(release_folder))
+        run('tar -xzf /tmp/{} -C {}'.format(archive_filename, release_folder))
+
+        # Deletes archive from the web server and move files specified dir:
+        run('rm /tmp/{}'.format(archive_filename))
+        run('mv {}web_static/* {}'.format(release_folder, release_folder))
+
+        run('rm -f /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(release_folder))
+
+        print('New version deployed!')
+        return True
+
+    except Exception:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
-        return False
-    return True
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("archive_path", type=str,
+                        help="path to the archive file")
+    parser.add_argument("-u", "--username", type=str,
+                        help="SSH username")
+    parser.add_argument("-i", "--private-key", type=str,
+                        help="Path to SSH private key")
+    args = parser.parse_args()
+
+    if args.username:
+        env.user = args.username
+
+    if args.private_key:
+        env.key_filename = args.private_key
+
+    do_deploy(args.archive_path)
