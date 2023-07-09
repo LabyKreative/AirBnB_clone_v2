99acdbe2cd4b6b6de4a4c6c8d41c368ad40af155
LabyKreative <labykreative@yahoo.com>
2023-07-09 16:41:05 +0000

    Update11: Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,24 +3,28 @@
 # web_static folder of your AirBnB Clone repo, using the function do_pack.
 import os
 from datetime import datetime
-from fabric.api import local
+from fabric.api import local, runs_once
 
 
+@runs_once
 def do_pack():
     """Create an archive file of the web_static directory"""
-    dt = datetime.utcnow().strftime('%Y%m%d%H%M%S')
-    arc_file = "versions/web_static_{}.tgz".format(dt)
-
-    print("Packing web_static to {}".format(arc_file))
-
-    if not os.path.exists("versions"):
-        if local("mkdir -p versions").failed:
-            return None
-
-    fab_file = local("tar -cvzf {} web_static".format(arc_file))
-    if fab_file.failed:
-        return None
-
-    size = os.path.getsize(arc_file)
-    print("web_static packed: {} -> {} Bytes".format(arc_file, size))
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    dt = datetime.now()
+    arc_file = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        dt.year,
+        dt.month,
+        dt.day,
+        dt.hour,
+        dt.minute,
+        dt.second
+    )
+    try:
+        print("Packing web_static to {}".format(arc_file))
+        local("tar -cvzf {} web_static".format(arc_file))
+        file_size = os.stat(arc_file).st_size
+        print("web_static packed: {} -> {} Bytes".format(arc_file, file_size))
+    except Exception:
+        arc_file = None
     return arc_file
