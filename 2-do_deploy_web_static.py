2516c9e421054a92236de98694efb73f76a71ea8
LabyKreative <labykreative@yahoo.com>
2023-07-11 23:40:59 +0000

    Do pack update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -29,9 +29,6 @@ def do_pack():
     else:
         return None
 
-    # Return the path to the archive file.
-    return arc_file_path
-
 
 # Deploy archive
 def do_deploy(archive_path):
@@ -39,16 +36,16 @@ def do_deploy(archive_path):
 
     if os.path.exists(archive_path):
         archived_file = archive_path[9:]
-        new_ver = "/data/web_static/releases/" + archived_file[:-4]
+        new_version = "/data/web_static/releases/" + archived_file[:-4]
         archived_file = "/tmp/" + archived_file
         put(archive_path, "/tmp/")
         run("sudo mkdir -p {}".format(new_ver))
-        run("sudo tar -xzf {} -C {}/".format(archived_file, new_ver))
+        run("sudo tar -xzf {} -C {}/".format(archived_file, new_version))
         run("sudo rm {}".format(archived_file))
-        run("sudo mv {}/web_static/* {}".format(new_ver, new_ver))
-        run("sudo rm -rf {}/web_static".format(new_ver))
+        run("sudo mv {}/web_static/* {}".format(new_version, new_version))
+        run("sudo rm -rf {}/web_static".format(new_version))
         run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(new_ver))
+        run("sudo ln -s {} /data/web_static/current".format(new_version))
 
         print("Yay! Alfeenah, new version deployed!")
         return True
