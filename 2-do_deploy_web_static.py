cd6ed0c2c5595cc708c34e3e93d0d8087fd6769c
LabyKreative <labykreative@yahoo.com>
2023-07-11 05:53:16 +0100

    Debugging_1

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,33 +11,48 @@ env.user = "ubuntu"
 
 def do_pack():
     """Create an archive file of the web_static directory"""
+
+    # Create a folder to store the archive file.
     local("mkdir -p versions")
+    logging.info("Folder versions/ created")
+
+    # Use the current date and time to name the archive file.
     date = datetime.now().strftime("%Y%m%d%H%M%S")
     arc_f_path = "versions/web_static_{}.tgz".format(date)
-    t_gzip_arc = local("tar -cvzf {} web_static".format(arc_f_path))
 
-    if t_gzip_arc.succeeded:
-        return arc_f_path
-    else:
-        return None
+    # Create the archive file.
+    local("tar -cvzf {} web_static".format(arc_f_path))
+    logging.info("Archive file {} created".format(arc_f_path))
+
+    # Return the path to the archive file.
+    return arc_f_path
 
 
+# Deploy archive
 def do_deploy(archive_path):
     """Sends an archive to a web server"""
     if os.path.exists(archive_path):
         arcd_file = archive_path[9:]
         new_ver = "/data/web_static/releases/" + arcd_file[:-4]
         arcd_file = "/tmp/" + arcd_file
-        put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}".format(new_ver))
-        run("sudo tar -xzf {} -C {}/".format(arcd_file, new_ver))
-        run("sudo rm {}".format(arcd_file))
-        run("sudo mv {}/web_static/* {}".format(new_ver, new_ver))
-        run("sudo rm -rf {}/web_static".format(new_ver))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(new_ver))
 
-        print("New version deployed!")
+        put(archive_path, "/tmp/")
+        print("Archive {} uploaded to /tmp/{}".format(arcd_file, arcd_file))
+        run("sudo mkdir -p {}".format(new_ver))
+        print("Folder {} created".format(new_ver))
+        run("sudo tar -xzf {} -C {}/".format(arcd_file, new_ver))
+        print("Uncompressed {} to {}".format(arcd_file, new_ver))
+        run("sudo rm {}".format(arcd_file))
+        print("Deleted {}".format(arcd_file))
+        run("sudo mv {}/web_static/* {}".format(new_ver, new_ver))
+        print("Moved content of {}/web_static/ to {}".format(new_ver, new_ver))
+        run("sudo rm -rf {}/web_static".format(new_ver))
+        print("Deleted folder {}/web_static/".format(new_ver))
+        run("sudo rm -rf /data/web_static/current")
+        print("Deleted /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(new_ver))
+        print("Created symlink {} -> {}".format(new_ver, current))
+
         return True
 
     return False
