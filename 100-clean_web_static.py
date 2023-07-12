0a61a09665dc686507ec711dd9c941b815944bc3
LabyKreative <labykreative@yahoo.com>
2023-07-12 02:25:50 +0000

    Fix: Keep it clean!

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -12,12 +12,12 @@ def do_clean(number=0):
     number = 1 if int(number) == 0 else int(number)
 
     archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
+    [archives.pop() for x in range(number)]
     with lcd("versions"):
         [local("rm ./{}".format(a)) for a in archives]
 
     with cd("/data/web_static/releases"):
         archives = run("ls -tr").split()
         archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
+        [archives.pop() for x in range(number)]
         [run("rm -rf ./{}".format(a)) for a in archives]
