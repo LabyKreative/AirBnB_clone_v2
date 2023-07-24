c77d69d311871be157883887598dc8955ff5a97b
LabyKreative <labykreative@yahoo.com>
2023-07-24 02:54:48 -0700

    Update: List of states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,5 +1,28 @@
 #!/usr/bin/python3
-"""script that starts a Flask web application."""
+"""script that starts a Flask web application
+web application must be listening on 0.0.0.0, port 5000
+You must use storage for fetching data from the storage engine
+(FileStorage or DBStorage) => from models import storage and storage.all(...)
+After each request you must remove the current SQLAlchemy Session:
+Declare a method to handle @app.teardown_appcontext
+Call in this method storage.close()
+Routes:
+/states_list: display a HTML page: (inside the tag BODY)
+H1 tag: “States”
+UL tag: with the list of all State objects present in DBStorage sorted
+by name (A->Z) tip
+LI tag: description of one State: <state.id>: <B><state.name></B>
+Import this 7-dump to have some data
+You must use the option strict_slashes=False in your route definition
+
+IMPORTANT:
+Make sure you have a running and valid setup_mysql_dev.sql in your
+AirBnB_clone_v2 repository (Task)
+Make sure all tables are created when you run:
+echo "quit" | HBNB_MYSQL_USER=hbnb_dev HBNB_MYSQL_PWD=hbnb_dev_pwd
+HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_dev_db HBNB_TYPE_STORAGE=db
+./console.py
+"""
 from models import storage
 from flask import Flask
 from flask import render_template
@@ -9,7 +32,7 @@ app = Flask(__name__)
 
 @app.route("/cities_by_states", strict_slashes=False)
 def cities_by_states():
-    """Displays an HTML page with a list of all states and related cities"""
+    """HTML page with a list of all states and related cities"""
     states = storage.all("State")
     return render_template("8-cities_by_states.html", states=states)
 
