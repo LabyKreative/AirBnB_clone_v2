64f33a54ddd9a7e296ffe6b881c857d5a903721a
LabyKreative <labykreative@yahoo.com>
2023-07-24 02:21:30 -0700

    Update: Number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
-"""script that starts a Flask web application."""
+"""script that starts a Flask web application:
+web application must be listening on 0.0.0.0, port 5000
+Routes:
+/: display “Hello HBNB!”
+/hbnb: display “HBNB”
+/c/<text>: display “C ”, followed by the value of the text variable
+(replace underscore _ symbols with a space )
+/python/(<text>): display “Python ”, followed by the value of the text
+variable (replace underscore _ symbols with a space )
+The default value of text is “is cool”
+/number/<n>: display “n is a number” only if n is an integer
+/number_template/<n>: display a HTML page only if n is an integer:
+H1 tag: “Number: n” inside the tag BODY
+You must use the option strict_slashes=False in your route definition
+"""
 from flask import Flask
 from flask import render_template
 
