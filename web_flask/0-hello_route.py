11b333a48a26557d0e11f8c6df1981e23b1d64ac
LabyKreative <labykreative@yahoo.com>
2023-08-17 09:05:12 +0000

    Set up dev with Python

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,16 +4,16 @@ the web application must be listening on 0.0.0.0, port 5000
 Routes: /: display “Hello HBNB!”
 You must use the option strict_slashes=False in your route definition
 """
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 app.debug = False
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/airbnb-onepage/", strict_slashes=False)
 def hello():
     """Displays 'Hello HBNB!'"""
-    return render_template("5-number.html")
+    return ("Hello HBNB!")
 
 
 if __name__ == "__main__":
